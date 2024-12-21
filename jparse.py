6dc65ad57710c22f95e8c9ec829a527099b79adb
programmerxero <programmerxero@gmail.com>
2024-12-21 18:21:59 +0330

    add tokenizer

--- jparse.py
+++ jparse.py
@@ -4,6 +4,38 @@ class JsonDecodeError(Exception):
         super().__init__(msg)
         self.message = msg
 
+def tokenize(json_str):
+    tokens = []
+    special_characters = "{}[],:"
+    whitespace_characters = "\n\t \r"
+    str_len = len(json_str)
+    
+    i = 0 #traversal pointer
+    while i < str_len:
+        char = json_str[i]
+        if char in whitespace_characters:
+            pass # ignore valid whitespace
+        elif char in special_characters: # special characters
+            tokens.append(char)
+        elif char == "\"": # handle strings
+            tmp = ""
+            while i < str_len:
+                tmp += json_str[i]
+                i += 1
+                if json_str[i] == "\"" and json_str[i - 1] != "\\":
+                    tmp += "\""
+                    break
+            tokens.append(tmp)
+        else: # handle other
+            tmp = ""
+            while i < str_len and (json_str[i] not in special_characters and json_str[i] not in whitespace_characters):
+                tmp += json_str[i]
+                i += 1
+            tokens.append(tmp)
+            continue
+        i += 1
+    return tokens
+
 def loads(json_str):
     data = {"data": None}
     container_openers = ["{", "["]
@@ -18,6 +50,9 @@ def loads(json_str):
     # no data
     if i >= str_len:
         return data
+    
+    # 0 for dict, 1 for array, -1 out of bounds
+    state = -1
 
     # check outer container
     if json_str[i] not in container_openers:
@@ -26,9 +61,14 @@ def loads(json_str):
     # traverse the string
     while i < str_len:
         if json_str[i] in container_openers: # add container to stack
+            if state >= 0 and len(stack) <= 0:
+                raise JsonDecodeError(f"Unexpected character {json_str[i]}")
             stack.append(json_str[i])
-        elif json_str[i] in container_closers:
-            if stack[-1] != container_openers[container_closers.index(json_str[i])]:
+            state = container_openers.index(json_str[i])
+        elif json_str[i] in container_closers: # close container
+            if len(stack) < 1:
+                raise JsonDecodeError(f"Unexpected character {json_str[i]}")
+            elif stack[-1] != container_openers[container_closers.index(json_str[i])]:
                 raise JsonDecodeError(f"{stack.pop()} was never closed")
             stack.pop()
         i += 1
